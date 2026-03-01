c123d41544277e873ff0b6b59cbd848a8381dafa
Your Name <you@example.com>
2026-03-01 21:14:26 +0100

    vmarzo

--- scripts/check_insiders.py
+++ scripts/check_insiders.py
@@ -11,7 +11,7 @@ import pickle
 #current_year = now.year
 #current_month = now.month
 current_year = 2026
-current_month = 2
+current_month = 3
 
 # --- Variables de entorno ---
 FINNHUB_KEY = os.environ.get("FINNHUB_KEY")
